ef1aab950cb328b2b82aff908e395a471a417bb0
sigkarmoil <haezers@gmail.com>
2020-08-15 11:28:13 -0400

    Finished

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import numpy as np
 
 #initialize browser to make this usable
 
@@ -90,6 +91,45 @@ def scrape():
     table_df = table_df.rename(columns = {0:"",1:"Value"})
     html_table = table_df.to_html(bold_rows = True, index = False)
     
+## Get Mars hemisphere picture ##
+    browser = init_browser()
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+        #if you need to give space
+    time.sleep(1)
+
+            # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "lxml")
+
+        #Scrape all the link that contains enhanced from div class="enhanced"
+    stripped_href=[]
+    stripped = soup.find_all('a', class_='itemLink product-item')
+    for strip in stripped:
+        stripped_href.append(f"https://astrogeology.usgs.gov{str(strip['href'])}" )
+    unq_href = np.unique(stripped_href)
+    
+
+# Visit each unique URL
+    stripped_href=[]
+    for unq in unq_href:
+            url = unq
+            browser = init_browser()
+            browser.visit(url)
+
+                #if you need to give space
+            time.sleep(1)
+
+                    # Scrape page into Soup
+            html = browser.html
+            soup = bs(html, "lxml")
+
+                    #Scrape all the link that contains enhanced from div class="enhanced"
+            
+            stripped = soup.find_all('li')
+                    # get the image link in download page
+            stripped_href.append(stripped[0].a.get('href'))
 
 ## Store data in dictionary ##
     scrape_data = {
@@ -97,7 +137,11 @@ def scrape():
         "latest_teaser": latest_teaser,
         "featured_image_url": featured_image_url,
         "twitter_scrape":mars_weather,
-        "html_table":html_table
+        "html_table":html_table,
+        "hemi_img0":stripped_href[0],
+        "hemi_img1":stripped_href[1],
+        "hemi_img2":stripped_href[2],
+        "hemi_img3":stripped_href[3]
     }
 
 # Close the browser after scraping
